e68e0952f80c15414b2cb31bed032cba76fabbfb
Jasmine+Matt <matt+jasmine@appacademy.io>
2015-01-26 13:41:53 -0500

    Game movement methods

--- game.rb
+++ game.rb
@@ -2,5 +2,33 @@ require 'dispel'
 require './minesweeper_board.rb'
 
 class Game
+  attr_reader :position
+
+  def initialize(rows, columns, bombs)
+    @board = Board.new(rows, columns, bombs)
+    @row_count = rows
+    @coulmn_count = columns
+    @position = [0, 0]
+  end
+
+  def up
+    @position[0] -= 1 unless @position[0] == 0
+  end
+
+  def down
+    @position[0] += 1 unless @position[0] ==  @row_count - 1
+  end
+
+  def left
+    @position[1] -= 1 unless @position[1] == 0
+  end
+
+  def right
+    @position[1] += 1 unless @position[1] ==  @column_count - 1
+  end
+end
+
+class Display
+
 
 end
