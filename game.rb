aa3d147868224eb326cbb9a31071b1d5396d7413
Jasmine+Matt <matt+jasmine@appacademy.io>
2015-01-26 13:47:19 -0500

    create display class

--- game.rb
+++ game.rb
@@ -2,7 +2,7 @@ require 'dispel'
 require './minesweeper_board.rb'
 
 class Game
-  attr_reader :position
+  attr_reader :position, :board
 
   def initialize(rows, columns, bombs)
     @board = Board.new(rows, columns, bombs)
@@ -29,6 +29,35 @@ class Game
 end
 
 class Display
+  def run
+    game = Game.new(9,9,10)
 
+    Dispel::Screen.open do |screen|
+      screen.draw(game.board.display, [], game.position)
+
+      Dispel::Keyboard.output do |key|
+        case key
+        when :up then game.up
+        when :left then game.left
+        when :right then game.right
+        when :down then game.down
+        when "q" then break
+        when "r" then game.board.reveal(game.position)
+        when "f" then game.board.flag(game.position)
+        end
+
+      screen.draw(game.board.display, [], game.position)
+      end
+    end
+  end
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+
+  display = Display.new
+
+  display.run
 
 end
